e4be4bea6f32151f15816debf6a91193a6116ee8
DKovale <dkov@ukr.net>
2019-06-25 22:48:49 +0300

    Updates

---
+++ precache-manifest.f66d2c5de796e121262ffc80c7ecbdd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "897c3428f783b1c70718da9fa0216e8a",
+    "url": "/react-static-todo/index.html"
+  },
+  {
+    "revision": "1cfe63a5622576e42df7",
+    "url": "/react-static-todo/static/css/main.3c067a89.chunk.css"
+  },
+  {
+    "revision": "0d93b29b27d1d199691f",
+    "url": "/react-static-todo/static/js/2.95678282.chunk.js"
+  },
+  {
+    "revision": "1cfe63a5622576e42df7",
+    "url": "/react-static-todo/static/js/main.d2f4afdf.chunk.js"
+  },
+  {
+    "revision": "7b989f3546c7b472a5c2",
+    "url": "/react-static-todo/static/js/runtime~main.81758ec1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-static-todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
